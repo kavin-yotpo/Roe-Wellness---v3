b967801417fc17494e3de0792a9d83269ca5170e
Kavin Abregana <abreganakavin@gmail.com>
2024-06-06 06:45:23 +0800

    updating product id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "7699702120625";
+  let ProductId = "7464987230385";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
